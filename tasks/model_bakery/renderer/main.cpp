6467065f111147abf5af9d8b503a0bc49fae8445
JackOfBlades232 <petrkiyashko232@gmail.com>
2024-10-20 23:27:10 +0300

    Quant WIP

--- tasks/model_bakery/renderer/main.cpp
+++ tasks/model_bakery/renderer/main.cpp
@@ -1,8 +1,22 @@
 #include "App.hpp"
+#include "GlobalSettings.hpp"
 
+#include <etna/Assert.hpp>
 
-int main()
+#include <cstring>
+
+GlobalSettings gSettings{};
+
+int main(int argc, char **argv)
 {
+  for (int i = 1; i < argc; ++i)
+  {
+    if (strncmp(argv[i], "-quantizedScene", 15) == 0)
+      gSettings.useQuantizedScene = true;
+    else
+      ETNA_PANIC("Invalid arg");
+  }
+
   {
     App app;
     app.run();
