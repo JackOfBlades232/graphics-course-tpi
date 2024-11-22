b8f0969d459a649aecbb0967ba249c5a714d41af
JackOfBlades232 <petrkiyashko232@gmail.com>
2024-11-22 20:24:59 +0100

    Well that was embarassing)))

--- tasks/model_bakery/common/quantization.h
+++ tasks/model_bakery/common/quantization.h
@@ -1,16 +1,19 @@
 #ifndef QUANTIZATION_H_INCLUDED
 #define QUANTIZATION_H_INCLUDED
 
+#include <bit>
 #ifdef __cplusplus
 
 #include <glm/glm.hpp>
 
-inline uint32_t quantize3fnorm(glm::vec3 in)
+inline uint32_t quantize4fnorm(glm::vec4 in)
 {
-  int32_t x = (int32_t)(in.x * 127.0) & 0xFF;
-  int32_t y = (int32_t)(in.y * 127.0) & 0xFF;
-  int32_t z = (int32_t)(in.z * 127.0) & 0xFF;
-  return (z << 16) | (y << 8) | x;
+  int8_t coords[4] = {
+    (int8_t)roundf(in.x * 127.f),
+    (int8_t)roundf(in.y * 127.f),
+    (int8_t)roundf(in.z * 127.f),
+    (int8_t)roundf(in.w * 127.f)};
+  return std::bit_cast<uint32_t>(coords);
 }
 
 #else
@@ -21,17 +24,17 @@ vec3 dequantize3fnorm(uint q)
   const uint a_enc_y = ((q & 0x0000FF00u) >> 8);
   const uint a_enc_z = ((q & 0x00FF0000u) >> 16);
 
-  const int usX = int(a_enc_x & 0x000000FFu);
-  const int usY = int(a_enc_y & 0x000000FFu);
-  const int usZ = int(a_enc_z & 0x000000FFu);
+  const uint usX = uint(a_enc_x & 0x000000FFu);
+  const uint usY = uint(a_enc_y & 0x000000FFu);
+  const uint usZ = uint(a_enc_z & 0x000000FFu);
 
-  const int sX = (usX <= 127) ? usX : usX - 256;
-  const int sY = (usY <= 127) ? usY : usY - 256;
-  const int sZ = (usZ <= 127) ? usZ : usZ - 256;
+  const int sX = (usX <= 127) ? usX : (int(usX) - 256);
+  const int sY = (usY <= 127) ? usY : (int(usY) - 256);
+  const int sZ = (usZ <= 127) ? usZ : (int(usZ) - 256);
 
-  const float x = sX*(1.0f / 127.0f);
-  const float y = sY*(1.0f / 127.0f);
-  const float z = sZ*(1.0f / 127.0f);
+  const float x = max(float(sX) / 127.0f, -1.0);
+  const float y = max(float(sY) / 127.0f, -1.0);
+  const float z = max(float(sZ) / 127.0f, -1.0);
 
   return vec3(x, y, z);
 }
