25bb8c89bcd9b94f1a16df96cf6602c7a614a884
JackOfBlades232 <petrkiyashko232@gmail.com>
2025-06-02 13:56:14 +0300

    SAT culling, harness done, broken WIP

--- common/render_utils/shaders/constants.h
+++ common/render_utils/shaders/constants.h
@@ -2,11 +2,14 @@
 #define CONSTANTS_H_INCLUDED
 
 #include "cpp_glsl_compat.h"
+#include "geometry.h"
 
 struct Constants
 {
   shader_mat4 mProjView;
   shader_mat4 mView;
+  ViewFrustum viewFrustum;
+  CullingMode cullingMode;
 };
 
 #endif // CONSTANTS_H_INCLUDED
