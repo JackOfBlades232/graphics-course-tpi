250f7a3f017cf1a095058b6d6ef4c490d33e3224
JackOfBlades232 <petrkiyashko232@gmail.com>
2026-04-10 00:21:28 +0300

    Remove length and sf from grass vs -- seems like a much bigger win (no sfu instructions in the hot grass vertex shader)

--- common/render_utils/shaders/grass.h
+++ common/render_utils/shaders/grass.h
@@ -8,22 +8,21 @@
   int pad_;                                                                                        \
   shader_vec2 chunkOrigins[];
 
-#define GRASS_ANGLE_BITS 9
-#define GRASS_ID_BITS (32 - GRASS_ANGLE_BITS)
-#define GRASS_ANGLE_MASK (((1 << GRASS_ANGLE_BITS) - 1) << GRASS_ID_BITS)
-#define GRASS_ID_MASK ((1 << GRASS_ID_BITS) - 1)
+#define GRASS_ID_BITS 3
+#define GRASS_SF_BITS (32 - GRASS_ID_BITS)
+#define GRASS_ID_MASK (((1 << GRASS_ID_BITS) - 1) << GRASS_SF_BITS)
+#define GRASS_SF_MASK ((1 << GRASS_SF_BITS) - 1)
 
 struct GrassInstance
 {
   shader_vec3 pos;
-  shader_uint idAndAnglePacked;
+  shader_uint idAndSf;
   shader_vec3 windOffset;
-  shader_uint pad0_;
+  shader_uint dirQuantized;
 };
 
-#define GRASS_INSTANCE_ANGLE(inst_)                                                                \
-  (float((inst_).idAndAnglePacked >> GRASS_ID_BITS) * SHADER_PI / 180.f)
-#define GRASS_INSTANCE_ID(inst_) ((inst_).idAndAnglePacked & GRASS_ID_MASK)
+#define GRASS_INST_GET_ID(inst_) ((inst_).idAndSf >> GRASS_SF_BITS)
+#define GRASS_INST_GET_SF(inst_) (uintBitsToFloat(((inst_).idAndSf & GRASS_SF_MASK) << (GRASS_ID_BITS - 1)))
 
 #define GRASS_SANK_PORTION 0.15f
 
